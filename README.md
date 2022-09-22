d7a4be04138cf55f21f1529bce09a302951004a2
Thully1131 <thulile1131@gmail.com>
2022-09-22 18:52:09 +0200

    Adding html,css and javascript code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Array-manipulation
+
+- Run the index.html file- if using Visual Studio Code open it with a live server.
+- You can input a name in the input field and click the add student button and that name will be added to the empty array.
+- The name will be displayed on the web page along with it's corresponding remove, edit and save button.
+- If you want to remove a name in the array, just click the remove button.
+- If you want to edit or change a name in the array, just click the edit button and that name will pop up on the input field.
+- You can then change/edit the name in the input field and click the save button to save the new name in the array.
---
+++ index.js
@@ -0,0 +1,44 @@
+var arrStudents = [];
+
+var holder = document.getElementById('studentList');
+
+var showList = function () {
+    holder.innerHTML = '';
+    for (i = 0; i < arrStudents.length; i++) {
+        holder.innerHTML = holder.innerHTML + '<p>' + arrStudents[i] + '</p> <button onclick="removeStudent(' + i + ')">Remove</button> <button onclick="editStudent('+ i +')"> edit</button> <button onclick="saveStudent('+ i +')"> save </button>'
+    }
+}
+
+showList();
+
+var studentName = document.getElementById('student')
+
+function addToList() {
+    arrStudents.push(studentName.value)
+    showList()
+    console.log(arrStudents)
+}
+function removeStudent(idx) {
+    arrStudents.splice(idx, 1)
+    showList()
+    console.log(arrStudents)
+}
+
+function editStudent(idx) {
+    studentName.value = arrStudents[idx]
+    showList()
+}
+ 
+ function saveStudent(idx) {
+    selectedIndex = idx
+    arrStudents.splice(selectedIndex, 1, studentName.value );
+    showList()
+    console.log(arrStudents)
+ }
+
+ 
+
+
+
+
+
